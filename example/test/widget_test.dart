cb8b5713a73619fc07206653d18752c832fae481
RandyWei <smile561607154@163.com>
2019-05-22 16:59:47 +0800

    导包报错

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,20 +8,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flt_video_player_example/main.dart';
-
-void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that platform version is retrieved.
-    expect(
-      find.byWidgetPredicate(
-        (Widget widget) => widget is Text &&
-                           widget.data.startsWith('Running on:'),
-      ),
-      findsOneWidget,
-    );
-  });
-}
+void main() {}
